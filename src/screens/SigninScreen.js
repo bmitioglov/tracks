6661e79ffd09b0b7cf09e2fabe18a624d12f4777
Boris Mitioglov <boris_mitioglov@epam.com>
2021-08-03 13:34:12 -0700

    Signup + Signin

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View, StyleSheet, Text} from 'react-native';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
+import {Context} from '../context/AuthContext'
+import {NavigationEvents} from 'react-navigation';
 
 const SigninScreen = () => {
-  return <Text style={{ fontSize: 48 }}>SigninScreen</Text>
+  const {state, signin, clearErrorMessage} = useContext(Context);
+  
+  return (
+    <View style={styles.container}>
+      <NavigationEvents onWillFocus={clearErrorMessage}/>
+      <AuthForm
+        headerText="Sign In to Your Account"
+        errorMessage={state.errorMessage}
+        submitButtonText="Sign In"
+        onSubmit={signin}/>
+      <NavLink routeName="Signup" text="Don't have an account? Sign up instead" />
+    </View>
+  )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 150
+  }
+});
+
+SigninScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
 
 export default SigninScreen;
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,35 +1,25 @@
-import React, {useState} from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState, useContext} from 'react';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
 import {Text, Input, Button} from 'react-native-elements';
 import Spacer from '../components/Spacer';
+import {Context as AuthContext} from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
+import {NavigationEvents} from 'react-navigation';
 
 const SignupScreen = ({navigation}) => {
+  const {state, signup, clearErrorMessage} = useContext(AuthContext);
   
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   
   return (
     <View style={styles.container}>
-      <Spacer>
-        <Text h3>Sign Up for Tracker</Text>
-      </Spacer>
-      <Input
-        label="Email"
-        value={email}
-        onChangeText={newEmail => setEmail(newEmail)}
-        autoCapitalize="none"
-        autoCorrect={false}/>
-      <Spacer/>
-      <Input
-        secureTextEntry
-        label="Password"
-        value={password}
-        onChangeText={setPassword}
-        autoCapitalize="none"
-        autoCorrect={false}/>
-      <Spacer>
-        <Button title="Sign Up"/>
-      </Spacer>
+      <NavigationEvents onWillFocus={clearErrorMessage} />
+      <AuthForm
+        headerText="Sign Up for Tracker"
+        errorMessage={state.errorMessage}
+        submitButtonText="Sign Up"
+        onSubmit={signup}/>
+      <NavLink routeName="Signin" text="Already have an account? Sign in instead!" />
     </View>
   );
 };
